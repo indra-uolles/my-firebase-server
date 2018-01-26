ab37ef24ac73c9e9e768a3d56ac72df476339d55
indra-uolles <smirnovanatalia2008@gmail.com>
2018-01-26 23:42:05 +0300

    projects&workexp

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const projects = require('./routes/projects');
+const workexp = require('./routes/workexp');
+const captchacheck = require('./routes/captchacheck');
 
 const app = express();
-app.get('/timestamp', (request, response) => {
-    response.send(`${Date.now()}`);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+app.use('/projects', projects);
+app.use('/workexp', workexp);
+app.use('/captchacheck', captchacheck);
 
 exports.app = functions.https.onRequest(app);
