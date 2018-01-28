088d825b22a8c941740628137ed902bf37ebea82
indra-uolles <smirnovanatalia2008@gmail.com>
2018-01-28 10:10:33 +0300

    captchacheck

--- functions/routes/captchacheck.js
+++ functions/routes/captchacheck.js
@@ -3,21 +3,16 @@ const router = express.Router();
 const got = require('got');
 
 router.post('/', function(req, res, next) {
-    // (async () => {
-    //     try {
-    //         const response = await got(
-    //             'https://www.google.com/recaptcha/api/siteverify?secret=' +
-    //             '6LdZzkEUAAAAAIR-vFLZ3a8OQc8Ixi03O1diG8dF' +
-    //             '&response=' + req.body.captcha
-    //         ).then(function(response) {
-    //             return res.json({
-    //                 success: JSON.parse(response.body).success
-    //             });
-    //         });
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // })();
+    got('https://www.google.com/recaptcha/api/siteverify?secret=' + '6LdZzkEUAAAAAIR-vFLZ3a8OQc8Ixi03O1diG8dF' + '&response=' + req.body.captcha)
+        .then(function(resp) {
+            return res.send({ success: true });
+            //res.send({ success: JSON.parse(resp.body).success });
+            //return ({ success: JSON.parse(resp.body).success });
+        })
+        .catch(function(err){
+            console.error(err);
+            res.send(err, 500)
+        });
 });
 
 module.exports = router;
