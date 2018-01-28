898e7f989b10585bd779efe6c7bf7dea3793fff1
indra-uolles <smirnovanatalia2008@gmail.com>
2018-01-28 11:00:18 +0300

    forgotten

--- functions/routes/captchacheck.js
+++ functions/routes/captchacheck.js
@@ -5,9 +5,7 @@ const got = require('got');
 router.post('/', function(req, res, next) {
     got('https://www.google.com/recaptcha/api/siteverify?secret=' + '6LfIxUEUAAAAAKfbgahQiPX31boA14xkcGXA8IP_' + '&response=' + req.body.captcha)
         .then(function(resp) {
-            return res.send({ success: true });
-            //res.send({ success: JSON.parse(resp.body).success });
-            //return ({ success: JSON.parse(resp.body).success });
+            return res.send({ success: JSON.parse(resp.body).success });
         })
         .catch(function(err){
             console.error(err);
